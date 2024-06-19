d608b6db9173730f0cda3aa7edd86ac9f2a82426
Sheng <sisheng1998@gmail.com>
2024-06-19 21:30:26 +0800

    update styling

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ThemeProvider from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/toaster";
 import TopLoader from "@/components/top-loader";
+import Toaster from "@/components/ui/toaster";
 
 const Providers = ({ children }: { children: React.ReactNode }) => (
   <ThemeProvider
--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -104,7 +104,7 @@ const ToastDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Description
     ref={ref}
-    className={cn("text-sm opacity-90", className)}
+    className={cn("whitespace-pre-wrap text-sm opacity-90", className)}
     {...props}
   />
 ));
