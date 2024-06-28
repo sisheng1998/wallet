b832d01e95474dafdc52336fe7f0ed26b375f5c3
Sheng <sisheng1998@gmail.com>
2024-06-28 09:07:26 +0800

    add custom logger

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,12 @@
 import { drizzle } from "drizzle-orm/d1";
 import env from "@/lib/env";
 import * as schema from "./schema";
+import { Logger } from "./utils";
 
 const getDB = () => {
   const db = drizzle(env.DB, {
     schema,
-    logger: process.env.NODE_ENV === "development",
+    logger: process.env.NODE_ENV === "development" && new Logger(),
   });
 
   return db;
--- src/db/utils.ts
+++ src/db/utils.ts
@@ -1,4 +1,15 @@
-import { SQL, sql } from "drizzle-orm";
+import { Logger as DrizzleLogger, SQL, sql } from "drizzle-orm";
 import { AnySQLiteColumn } from "drizzle-orm/sqlite-core";
 
 export const lower = (text: AnySQLiteColumn): SQL => sql`lower(${text})`;
+
+export class Logger implements DrizzleLogger {
+  logQuery(query: string, params: unknown[]): void {
+    console.log(
+      "[Query]:",
+      query,
+      "\n--params",
+      `[${params.map((p) => JSON.stringify(p)).join(", ")}]`,
+    );
+  }
+}
