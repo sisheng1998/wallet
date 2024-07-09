f4ea7e28d51574c8dadba0c3133bdefc53ef0e21
Sheng <sisheng1998@gmail.com>
2024-07-09 23:05:24 +0800

    add bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev"
+import createBundleAnalyzer from "@next/bundle-analyzer"
 import createMDX from "@next/mdx"
 import remarkGfm from "remark-gfm"
 
@@ -11,10 +12,14 @@ const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 }
 
+const withBundleAnalyzer = createBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})
+
 const withMDX = createMDX({
   options: {
     remarkPlugins: [remarkGfm],
   },
 })
 
-export default withMDX(nextConfig)
+export default withBundleAnalyzer(withMDX(nextConfig))
