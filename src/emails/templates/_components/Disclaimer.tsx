295ab3892c2cb9e596e6a3a82c80920db29d12db
Sheng <sisheng1998@gmail.com>
2024-07-12 22:03:22 +0800

    update styling

--- src/emails/templates/_components/Disclaimer.tsx
+++ src/emails/templates/_components/Disclaimer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Text } from "@react-email/components"
 
 const Disclaimer = () => (
-  <Text className="text-xs leading-normal text-neutral-400">{`If you didn't request this email, there's nothing to worry about, you can safely ignore it.`}</Text>
+  <Text className="text-xs italic leading-normal text-neutral-400">{`If you didn't request this email, there's nothing to worry about, you can safely ignore it.`}</Text>
 )
 
 export default Disclaimer
