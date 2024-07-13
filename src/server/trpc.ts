c320093e3ba5a87e120ea86eaeb87a2677ff768d
Sheng <sisheng1998@gmail.com>
2024-07-13 22:04:02 +0800

    add protected route

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,13 +1,20 @@
-import { initTRPC } from "@trpc/server"
+import { initTRPC, TRPCError } from "@trpc/server"
 import superjson from "superjson"
 import { ZodError } from "zod"
 
 import { db } from "@/db"
+import { validateRequest } from "@/lib/auth"
 
-export const createTRPCContext = async (opts: { headers: Headers }) => ({
-  db,
-  ...opts,
-})
+export const createTRPCContext = async (opts: { headers: Headers }) => {
+  const { user, session } = await validateRequest()
+
+  return {
+    db,
+    session,
+    user,
+    ...opts,
+  }
+}
 
 const t = initTRPC.context<typeof createTRPCContext>().create({
   transformer: superjson,
@@ -25,3 +32,21 @@ export const createCallerFactory = t.createCallerFactory
 export const createTRPCRouter = t.router
 
 export const publicProcedure = t.procedure
+
+export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
+  if (!ctx.session || !ctx.user) throw new TRPCError({ code: "UNAUTHORIZED" })
+
+  return next({
+    ctx: {
+      session: { ...ctx.session },
+      user: { ...ctx.user },
+    },
+  })
+})
+
+export type TRPCContext = Awaited<ReturnType<typeof createTRPCContext>>
+
+export type ProtectedTRPCContext = TRPCContext & {
+  user: NonNullable<TRPCContext["user"]>
+  session: NonNullable<TRPCContext["session"]>
+}
